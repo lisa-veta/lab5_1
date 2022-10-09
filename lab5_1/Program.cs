c29b00906ba57a7dbff11754cd0602e1b47c1a7c
Elizaveta Pernikova <lisapernikowa@mail.ru>
2022-10-10 00:52:24 +0500

    рабочая вресия

--- lab5_1/Program.cs
+++ lab5_1/Program.cs
@@ -1,4 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Lab5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double lastChar = double.NaN;
 
+            while (true)
+            {
+                string input = Console.ReadLine();  
+
+                if (int.TryParse(input, out int number)) 
+                {
+                    Console.WriteLine(Convert.ToChar(number));
+                }
+
+                if ((Convert.ToDouble(input) == lastChar) || input == "q")
+                {
+                    break;
+                }
+                else
+                {
+                    lastChar = Convert.ToDouble(input);
+                }
+                
+            }
+        }
+    }
+}
